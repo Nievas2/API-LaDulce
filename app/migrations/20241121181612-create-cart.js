d612c201ecf74af9dcd2316f947116404cb4d86a
Nievas2 <angelgabrielnievas@gmail.com>
2024-11-21 15:37:21 -0300

    fix(Cart): :bug: Fix model and migration

--- app/migrations/20241121181612-create-cart.js
+++ app/migrations/20241121181612-create-cart.js
@@ -1,38 +1,41 @@
-'use strict';
-/** @type {import('sequelize-cli').Migration} */
+"use strict"
+
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Carts', {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Carts", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       ProductId: {
         type: Sequelize.INTEGER,
+        allowNull: false,
         onDelete: "CASCADE",
         onUpdate: "CASCADE",
       },
-      UserId: {
+      userId: {
         type: Sequelize.INTEGER,
+        allowNull: false,
         onDelete: "CASCADE",
         onUpdate: "CASCADE",
       },
       amount: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+        type: Sequelize.DATE,
+      },
+    })
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Carts');
-  }
-};
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Carts")
+  },
+}
--- app/models/cart.js
+++ app/models/cart.js
@@ -2,20 +2,12 @@
 const { Model } = require("sequelize")
 module.exports = (sequelize, DataTypes) => {
   class Cart extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
       Cart.belongsTo(models.Product, {
-        foreignKey: "id",
-        target_key: "ProductId",
+        foreignKey: "ProductId",
       })
       Cart.belongsTo(models.User, {
-        foreignKey: "id",
-        target_key: "userId",
+        foreignKey: "userId",
       })
     }
   }
